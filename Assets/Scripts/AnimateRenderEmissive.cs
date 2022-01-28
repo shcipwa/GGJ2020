b08d01f38fb0e2c767fc93f3a27b7aa8a8415b70
shcipwa <shcipwa@gmail.com>
2022-01-29 08:45:24 +1100

    Added grid manager to a tom scene

--- Assets/Scripts/AnimateRenderEmissive.cs
+++ Assets/Scripts/AnimateRenderEmissive.cs
@@ -7,15 +7,17 @@ public class AnimateRenderEmissive : MonoBehaviour
     private Renderer _renderer;
     private MaterialPropertyBlock _propBlock;
 
+    public float TriggerDelayRandom = 0.01f;
     public float TriggerDelay = 0.1f;
     public float MaxBrightness = 5f;
     public Vector2Int GridCoord;
     public Color Color1, Color2;
     public float FadeSpeed = 1f;
+    public float FadeSpeedRandom = 0.2f;
     public float FadePower = 1f;
     private float _value = 0;
 
-    private float _timeToTriggerAdjacent;
+    private float _timeToTriggerAdjacent = -1;
 
     public bool Trigger;
 
@@ -25,6 +27,7 @@ public class AnimateRenderEmissive : MonoBehaviour
     {
         _propBlock = new MaterialPropertyBlock();
         _renderer = GetComponent<Renderer>();
+        FadeSpeed += Random.value * FadeSpeedRandom;
     }
 
     // Update is called once per frame
@@ -32,12 +35,35 @@ public class AnimateRenderEmissive : MonoBehaviour
     { 
         if (Trigger)
         {
-            _value = MaxBrightness;
-            Trigger = false;
+            TriggerCell();
         }
 
-        _value -= FadeSpeed * Time.time;
+        if (_timeToTriggerAdjacent > 0 && _timeToTriggerAdjacent < Time.time)
+        {
+            _timeToTriggerAdjacent = -1;
+            GridManager.Instance.TriggerAdjacent(GridCoord);
+        }
+
+        
+        _value -= FadeSpeed * Time.deltaTime;
+        if (_value < 0)
+        {
+            _value = 0;
+        }
         SetRenderer();
+    }
+    
+    public void TriggerCell()
+    {
+        if (_value > 0)
+        {
+            return;
+        }
+        
+        _value = 1;
+        Trigger = false;
+        
+        _timeToTriggerAdjacent = Time.time + TriggerDelay + Random.value * TriggerDelayRandom;
     }
 
     private void SetRenderer()
@@ -45,14 +71,8 @@ public class AnimateRenderEmissive : MonoBehaviour
         _renderer.GetPropertyBlock(_propBlock);
         // Assign our new value.
         //_propBlock.SetColor("_EmissiveColor", Color.Lerp(Color1, Color2, (Mathf.Sin(Time.time * Speed + Offset) + 1) / 2f));
-        _propBlock.SetFloat("_EmissiveAmount", Mathf.Pow(_value,FadePower));
+        _propBlock.SetFloat("_EmissiveAmount", Mathf.Pow(_value,FadePower) * MaxBrightness);
         // Apply the edited values to the renderer.
         _renderer.SetPropertyBlock(_propBlock);
-    }
-
-    public void TriggerCell()
-    {
-        _timeToTriggerAdjacent = Time.time + TriggerDelay;
-        
     }
 }
--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class GridManager : Singleton<GridManager>
 {
@@ -13,7 +14,7 @@ public class GridManager : Singleton<GridManager>
     // Start is called before the first frame update
     void Start()
     {
-        Vector3 startPoint = transform.position;
+        Vector3 startPoint = transform.position - new Vector3((GridSize * GridSpacing) / 2f,0,(GridSize * GridSpacing) / 2f);
         
         GridCells = new AnimateRenderEmissive[GridSize][];
         for (var i = 0; i < GridSize; i++)
@@ -34,6 +35,32 @@ public class GridManager : Singleton<GridManager>
     // Update is called once per frame
     void Update()
     {
-        
+        if (Keyboard.current.fKey.wasPressedThisFrame)
+        {
+            TriggerCell(new Vector2Int((int)(Random.value * GridSize),(int)(Random.value * GridSize)));
+        }
+    }
+
+    public void TriggerAdjacent(Vector2Int gridCoord)
+    {
+        TriggerCell(gridCoord + Vector2Int.down);
+        TriggerCell(gridCoord + Vector2Int.down + Vector2Int.left);
+        TriggerCell(gridCoord + Vector2Int.left);
+        TriggerCell(gridCoord + Vector2Int.down + Vector2Int.right);
+        TriggerCell(gridCoord + Vector2Int.right);
+        TriggerCell(gridCoord + Vector2Int.up + Vector2Int.left);
+        TriggerCell(gridCoord + Vector2Int.up);
+        TriggerCell(gridCoord + Vector2Int.up + Vector2Int.right);
+    }
+
+    private void TriggerCell(Vector2Int gridCoord)
+    {
+        //Debug.Log("Attempt: " + gridCoord);
+        if (gridCoord.x >= GridSize || gridCoord.y >= GridSize || gridCoord.x < 0 || gridCoord.y < 0)
+        {
+            return;
+        }
+        //Debug.Log("Success: " + gridCoord);
+        GridCells[gridCoord.x][gridCoord.y].TriggerCell();
     }
 }
